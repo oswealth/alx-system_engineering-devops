c0c2a39b6b0135fb1bb023b1c0de35c620034cd5
oswealth <osbertjaywealth@gmail.com>
2024-03-25 09:30:25 -0700

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""using the JSONPlaceholder API:
+   returns information about the TODO list progress for a given employee ID"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todo = requests.get(url + "todos", params={"userId": argv[1]}).json()
+    tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(tasks), len(todo)))
+    for task in tasks:
+        print("\t {}".format(task))
