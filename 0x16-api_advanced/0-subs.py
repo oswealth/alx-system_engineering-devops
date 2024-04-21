1812e1a0ad21d187179b450a566bdd9789ae5cdc
oswealth <osbertjaywealth@gmail.com>
2024-04-21 15:12:07 -0700

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,16 +11,13 @@ def number_of_subscribers(subreddit):
     }
     try:
         response = requests.get(
-            f'https://www.reddit.com/r/{subreddit}/about.json',
-            headers=headers, allow_redirects=False)
+                f'https://www.reddit.com/r/{subreddit}/about.json',
+                headers=headers, allow_redirects=False)
         if response.status_code == 200:
             json_response = response.json()
             data_dict = json_response.get('data')
-            if data_dict and 'subscribers' in data_dict:
-                print("OK")  # Print "OK" if the subreddit exists
+            if data_dict:
                 return data_dict.get('subscribers', 0)
-        print("OK")  # Print "OK" even if the subreddit does not exist
         return 0
     except requests.RequestException:
-        print("OK")  # Print "OK" in case of a request exception
         return 0
