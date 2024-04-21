2ac14fd94f1c468ac39f015fb957cd9b989b19cb
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-04-21 19:18:58 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,14 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     headers = {
         'User-Agent': 'My User Agent 1.0'
     }
     try:
-        response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers, allow_redirects=False)
+        response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers,
+                                allow_redirects=False)
         if response.status_code == 200:
             json_response = response.json()
             data_dict = json_response.get('data')
